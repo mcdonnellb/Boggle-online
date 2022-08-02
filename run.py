4b90e8283f503f51672db6d9726c370fad122fc0
mcdonnellb <bernadettemichelemcdonnell@gmail.com>
2022-08-02 23:39:05 +0000

    adding class for boggle board, new game function

--- run.py
+++ run.py
@@ -1,3 +1,37 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+scores = {"computer": 0, "plarer": 0}
+
+class BoggleBoard: 
+    """
+    Main Boggle Board class. This sets the board size, the number of letters to the displayed in each round. This boards displays the contdown and has method to capture the users word input and validate against dictionary to award points for valid word entry only
+    """
+
+    def __init__(self, size, num_letters, score, word_count):
+        self.size = size
+        self.num_letters = num_letters
+        self.score = score
+        self.word_count = word_count
+        self.high_score = []
+        self.timer = 90
+
+    def print(self):
+
+def new_game():
+        
+        """
+            
+        This will start a new game. This sets the board size, resets the timer, the score and refreshes the letters on the board.
+            
+        """
+        size = 5
+        scores["player"] = 0
+        print("Welcome to Boggle Online")
+        print(f"Board Size: {size}"). Number of Letters: {}")
+
+
+
+new_game()
