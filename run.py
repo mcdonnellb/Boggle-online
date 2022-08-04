b956696e5b40a38e26fde25d6ad16500530543cf
mcdonnellb <bernadettemichelemcdonnell@gmail.com>
2022-08-04 13:31:39 +0000

    adding constants file and print statements

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import constants
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -10,9 +11,11 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+SHEET = GSPREAD_CLIENT.open('boggle_online')
 
 sales = SHEET.worksheet('sales')
+high_score = SHEET.worksheet('highscore')
+high_score_data = high_score.get_all_values()
 
 data = sales.get_all_values()
 
@@ -20,39 +23,57 @@ print(data)
 
 
 class BoggleBoard:
-      """
-            Main Boggle Board class.
-            This sets the board size,
-            the number of letters to the displayed.
-            This boards displays the contdown and
-            has method to capture the users word
-            input and validate against dictionary
-            to award points for valid word entry only
-            """
+    """
+    Main Boggle Board class.
+    This sets the board size,
+    the number of letters to the displayed.
+    This boards displays the contdown and
+    has method to capture the users word
+    input and validate against dictionary
+    to award points for valid word entry only
+    """
+
 
 def __init__(self, size, num_letters, score, word_count):
-self.size = size
-self.num_letters = num_letters
-self.score = score
-self.word_count = word_count
-self.high_score = []
-self.timer = 90
-print(self)
+    self.size = size
+    self.num_letters = num_letters
+    self.score = score
+    self.word_count = word_count
+    self.high_score = []
+    self.timer = 90
+    print(self)
 
-def new_game():
-        
 
+def welcome_screen():
     """
     This will start a new game.
     This sets the board size,
     resets the timer and the score
     and refreshes the letters on the board.
-   
     """
-size = 5
-num_letters = 16
-scores["player"] = 0
-print("Welcome to Boggle Online")
-print(f" Board Size: {size}. Number of Letters: {num_letters}")
-player_name = input("Please choose your username: \n")
-new_game()
+
+
+print('\n' constants.WELCOME '\n')
+
+player_selection = input("Please select Option H, S or Enter: \n")
+
+welcome_screen()
+
+
+def new_game():
+    """
+    This will start a new game.
+    This sets the board size,
+    resets the timer and the score
+    and refreshes the letters on the board.
+    """
+
+
+SIZE = 5
+
+NUM_LETTERS = 16
+# scores["player"] = 0
+# print(f" Board Size: {SIZE}. Number of Letters: {NUM_LETTERS}")
+# player_name = input("Please choose your username: \n")
+# new_game()
+# print(high_score_data)
