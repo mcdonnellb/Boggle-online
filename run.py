80e26424db40b50ce325c2d996eedbccd4c69c03
mcdonnellb <bernadettemichelemcdonnell@gmail.com>
2022-08-04 23:54:16 +0000

    creating separate function for show menu

--- run.py
+++ run.py
@@ -22,7 +22,6 @@ high_score_data = high_score.get_all_values()
 data = sales.get_all_values()
 
 
-
 class BoggleBoard:
     """
     Main Boggle Board class.
@@ -51,6 +50,8 @@ def generate_board():
     new random characters on the 
     board.
     """
+
+
 board_selection = SHEET.worksheet('letters')
 board_selection_data = board_selection.get_all_values()
 gen_random_sixteen = random.choices(board_selection)
@@ -86,18 +87,24 @@ SIZE = 5
 NUM_LETTERS = 16
 score = 0
 
-print(constants.WELCOME1)
-print(constants.WELCOME2)
+
 print("Can you find words on our online Boggle board?")
 print("If you get a high score you will be added to our Boggle Hall of fame")
 print("Until someone beats your score that is!!")
 print(f" Board Size: {SIZE}. Number of Letters: {NUM_LETTERS}")
 player_name = input("Please choose your username: \n")
 print("{player_name {words entered}")
-generateBoard()
+generate_board()
 #new_game()
 # print(high_score_data)
 
+def show_menu():
+print(constants.WELCOME1)
+print(constants.WELCOME2)
+print("++++ || For Help please hit the H Key      || ++++")
+print("++++ || For Scoreboard please hit the S Key|| ++++")
+print("++++ || To start a game hit Enter Key      || ++++")
+player_selection = input("Please select Option H, S or Enter: \n")
 
 def welcome_screen():
     """
@@ -113,24 +120,22 @@ print(constants.WELCOME2)
 print("Can you find words on our online Boggle board?")
 print("If you get a high score you will be added to our Boggle Hall of fame")
 print("Until someone beats your score that is!!")
-print("++++ || For Help please hit the H Key      || ++++")
-print("++++ || For Scoreboard please hit the S Key|| ++++")
-print("++++ || To start a game hit Enter Key      || ++++")
+show_menu()
 
-#player_selection = input("Please select Option H, S or Enter: \n")
+# player_selection = input("Please select Option H, S or Enter: \n")
 
 new_game()
 # code taken from www.codegrepper.com noted in readme
-#https://stackoverflow.com/questions/24072790/how-to-detect-key-presses
+# https://stackoverflow.com/questions/24072790/how-to-detect-key-presses
 
-#user_input = input()
-#options = ("H", "S", "E")
-#if user_input == "E":
- #   print("You Pressed Enter !/n")
-  #  welcome_screen()
-#if user_input not in options:
- #   print("uh oh")
-  #  error_in_selection()
+# user_input = input()
+# options = ("H", "S", "E")
+# if user_input == "E":
+# print("You Pressed Enter !/n")
+#  welcome_screen()
+# if user_input not in options:
+# print("uh oh")
+# error_in_selection()
 
 
 def error_in_selection():
@@ -145,53 +150,30 @@ def error_in_selection():
     print(constants.WELCOME1)
     print(constants.WELCOME2)
     print("++++ || You hit the wrong key        || ++++")
-    print("++++ || Choose Enter to start a game || ++++")
-    print("++++ || Choose S to view Scoreboard  || ++++")
-    print("++++ || Choose H for Help            || ++++")
-    player_selection = input("Please select Option H, S or Enter: \n")
+    show_menu()
    # elif keyboard.is_pressed('s'):
-    #        print(constants.SCOREBOARD)
-     #       # break  # finishing the loop
-      #      print(high_score_data)
-       # elif keyboard.is_pressed('h'):
-        #    print(constants.INSTRUCTIONS)
-         #   print("++++ || Boggle will generate 16 char grid || ++++")
-          #  print("++++ || Your goal is to form words        || ++++")
-           # print("++++ || They must be atleast 3 chars long || ++++")
-            # print("++++ || You have 90 seconds on the clock  || ++++")
-            # print("++++ || QU will count as two letters      || ++++")
-            #print("++++ || A 3-letter word earns you 1 point || ++++")
-            #print("++++ || A 5-letter word earns you 2 points|| ++++")
-            #print("++++ || A 6-letter word earns you 3 points|| ++++")
-            #print("++++ || A 7-letter word earns you 5 points|| ++++")
-            #print("++++ || 8+ letters earns you 11 points    || ++++")
+   # print(constants.SCOREBOARD)
+   # break  # finishing the loop
+   # print(high_score_data)
+     # elif keyboard.is_pressed('h'):
+     #    print(constants.INSTRUCTIONS)
+    #   print("++++ || Boggle will generate 16 char grid || ++++")
+    #  print("++++ || Your goal is to form words        || ++++")
+    # print("++++ || They must be atleast 3 chars long || ++++")
+    # print("++++ || You have 90 seconds on the clock  || ++++")
+    # print("++++ || QU will count as two letters      || ++++")
+    # print("++++ || A 3-letter word earns you 1 point || ++++")
+    # print("++++ || A 5-letter word earns you 2 points|| ++++")
+    # print("++++ || A 6-letter word earns you 3 points|| ++++")
+    # print("++++ || A 7-letter word earns you 5 points|| ++++")
+    # print("++++ || 8+ letters earns you 11 points    || ++++")
         
-        #break
+        # break
 
 
 
         
         # print(constants.WELCOME1)
-        #print(constants.WELCOME2)
-    
-
+        # print(constants.WELCOME2)
     #welcome_screen()
 
-
-def new_game():
-    """
-    This will start a new game.
-    This sets the board size,
-    resets the timer and the score
-    and refreshes the letters on the board.
-    """
-
-
-SIZE = 5
-
-NUM_LETTERS = 16
-# scores["player"] = 0
-# print(f" Board Size: {SIZE}. Number of Letters: {NUM_LETTERS}")
-# player_name = input("Please choose your username: \n")
-# new_game()
-# print(high_score_data)
