250fa175eb1ae642695c83a3e883e0d196a43abc
mcdonnellb <bernadettemichelemcdonnell@gmail.com>
2022-09-18 22:47:28 +0000

    splitting values entered and retrurning value

--- run.py
+++ run.py
@@ -21,19 +21,40 @@ print(high_score_data)
 
 def get_random_choice():
     return random.choices(board_selection)
+# function to calculate the points achieved in the course of the game
+def points_calculation(words_entered):
+    if len(words_entered) == 3:
+        return 1
+        print("your score is 1")
+    if len(words_entered) == 5:
+        return 2
+        print("your score is 2")
+    if len(words_entered) == 6:
+        return 3
+    if len(words_entered) == 7:
+        return 11
+    if len(words_entered) >= 8:
+        return 11
+    return 0
+
 
 
 def generate_board():
     board_selection = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'QU')
     grid = []
+    guesses = []
     
     for i in range(8):
         grid.append(random.choices(board_selection, k=8))
         print(grid[i])
     words_entered = input("Enter your words here: ")
+    guess_list = words_entered.split(",")
+    return guess_list 
+
 
 # code taken from www.codegrepper.com noted in readme
 # https://stackoverflow.com/questions/24072790/how-to-detect-key-presses
+
 
 def welcome_screen():
     """
@@ -42,15 +63,20 @@ def welcome_screen():
     run and will be the main
     page for nav purposes
     """
+
     print(constants.WELCOME1)
     print(constants.WELCOME2)
     print("Can you find words on our online Boggle board?")
     print("Get a high score to be added to the hall of fame")
+    print("++++ || For Help please hit the H Key      || ++++")
+    print("++++ || For Scoreboard please hit the S Key|| ++++")
+    print("++++ || To start a game hit Enter Key      || ++++")
+    player_selection = input("Please select Option H, S or Enter: \n")
     user_name = input(" Enter username:")
     print("Let's go " + user_name + '!')
     generate_board()
 welcome_screen()
-show_menu()
+
 
 
 def end_game():
@@ -79,31 +105,19 @@ def end_game():
         #print(score)
 
 
-# function to calculate the points achieved in the course of the game
-def points_calculation(words_entered):
-    if len(words_entered) == 3:
-        return 1
-    if len(words_entered) == 5:
-        return 2
-    if len(words_entered) == 6:
-        return 3
-    if len(words_entered) == 7:
-        return 11
-    if len(words_entered) >= 8:
-        return 11
-    return 0
 
-
-def check_if_high_score(score):
-    if score > 10:
+def update_worksheet(data, worksheet):
+     """
+    This function checks the score on the spreadsheet and if current score is higher updates the values 
+    """
+while True:
         current_score = SHEET.worksheet("scores").get_all_values()
         print(f"Updating {SHEET} worksheet...\n")
-        worksheet_to_update = SHEET.worksheet(scores)
+        worksheet_to_update = SHEET.worksheet("scores")
         worksheet_to_update.append_row(data)
         update_worksheet(high_score_data, "scores")
         print(f"{worksheet} ScoreBoard updated successfully\n")
         print(high_score_data)
-
 
 def show_menu():
 
@@ -134,4 +148,4 @@ def error_in_selection():
     print(constants.INPUT_ERROR2)
     print(constants.INPUT_ERROR3)
     print("++++ || You hit the wrong key        || ++++")
-    show_menu()
+    
