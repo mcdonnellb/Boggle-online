165a49d706b5e3310e4a07dbad58889e0a6444e1
mcdonnellb <bernadettemichelemcdonnell@gmail.com>
2022-09-22 09:51:33 +0000

    remove whitespace and indentation errors

--- run.py
+++ run.py
@@ -25,15 +25,18 @@ def get_random_choice():
 
 
 def generate_board():
-    board_selection = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'QU')
+    board_selection = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+                       'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+                       'S', 'T', 'U',
+                       'V', 'W', 'X', 'Y', 'Z', 'QU')
     grid = []
     guesses = []
-    score = 0 
-    
+    score = 0
+
     for i in range(8):
         grid.append(random.choices(board_selection, k=8))
         print(grid[i])
-    words_entered = input("Enter your words here with a space in between each: ")
+    words_entered = input("Enter your words with a space in between: ")
     if not re.match("^[a-z , ]*$", words_entered):
         print ("Error! Only  a-z selection allowed!")
         words_entered = input("Try again - Enter your words here: ")
@@ -43,26 +46,25 @@ def generate_board():
     guesses.append(words_entered)
     print("Here are the words you found", guess_list)
     print(len(words_entered))
-    return guess_list 
+    return guess_list
 # code taken from www.codegrepper.com noted in readme
 # https://stackoverflow.com/questions/24072790/how-to-detect-key-presses
 
 
 def end_game():
-   
     """
-    This function is to 
-    end the game, show the end game 
+    This function is to
+    end the game, show the end game
     graphics and call the crucial
     functions which determine
     if words entered are valid
     and if so the score
-    This will also determine if 
-     user has high score
-    which will then be pushed 
+    This will also determine if
+    user has high score
+    which will then be pushed
     to the google sheet
     """
-    
+
     print(constants.TIME_IS_UP1)
     print(constants.TIME_IS_UP2)
     print(constants.TIME_IS_UP3)
@@ -72,39 +74,44 @@ def end_game():
     welcome_screen()
 
 
-            
 def main_menu():
+    """
+    Function to allow user select menu options and navigation
+    """
     user_selection = input("Enter 'YES' to start a game. "
-    "Enter S for High Scores, H for Help Menu or any other UPPER letter to exit the game: "
-    )
+                           "Enter S for Scores, H for Help or "
+                           "any UPPER letter to exit the game: "
+                           )
     if not re.match("^[A-Z]*$", user_selection):
-       print ("Error! Only  a-z selection allowed!")
-       user_selection = input(" TRY AGAIN -Enter 'YES' to start a game. "
-             "Enter S for High Scores, H for Help Menu or any other UPPER letter to exit the game: "
-             )
+        print ("Error! Only  a-z selection allowed!")
+        user_selection = input(" \n TRY AGAIN -Enter 'YES' to start a game. "
+                               "Enter S for High Scores, H for Help or "
+                               " other UPPER letter to exit the game:\n "
+                               )
     if user_selection .upper() == 'YES' or user_selection .upper() == 'Y':
-       user_name = input(" Enter username:")
-       print("Let's go " + user_name + '!')   
-       generate_board()
-       end_game()
+        user_name = input(" Enter username:")
+        print("Let's go " + user_name + '!')
+        generate_board()
+        end_game()
     elif user_selection .upper() == 'H':
-         print("++++ || Welcome to the Help page                                                                             || ++++")
-         print("++++ || The aim of the game is simple                                                                        || ++++")
-         print("++++ || To find as many words as you can in the grid                                                         || ++++")
-         print("++++ || Can you find words upside down, diagnolly, hoirzontally? Let's kick that brain into action!          || ++++")
-         welcome_screen() 
+        print("++++ || Welcome to the Help page      || ++++")
+        print("++++ || The aim of the game is simple || ++++")
+        print("++++ || Find as many words as you can in the grid || ++++")
+        print("++++ || Upside down, diagnolly, hoirzontally || ++++")
+        print("++++ || Let's kick that brain into action! || ++++")
+        welcome_screen()
     elif user_selection .upper() == 'S':
         print(constants.SCOREBOARD)
-        print("++++ || Welcome to the High Score page                                                                  || ++++")
-        print("++++ || The Boggle Hall of fame                                                                         || ++++")
-        print("++++ || Do you have what it takes to beat our superstars?                                               || ++++")
+        print("++++ || Welcome to the High Score page || ++++")
+        print("++++ || The Boggle Hall of fame        || ++++")
+        print("++++ || Do you have what it takes    ? || ++++")
         high_score = SHEET.worksheet('scores')
         high_score_data = high_score.get_all_values()
         print(high_score_data)
         welcome_screen()
     else:
-        print("Thank you")  
-        print("You have opted to exit") 
+        print("Thank you")
+        print("You have opted to exit")
         sys.exit()
 
 
@@ -124,4 +131,3 @@ def welcome_screen():
     print("++++ || To reimagine the 90's classic            || ++++")
     main_menu()
 welcome_screen()
-    
